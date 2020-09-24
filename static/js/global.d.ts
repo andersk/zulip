05c291214cd9ff7d043ca464794e2d4532b26e1e
Anders Kaseorg <anders@zulip.com>
2020-09-24 12:02:58 +0000

    js: Convert static/js/settings_user_groups.js to ES6 module with modzulator.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- static/js/global.d.ts
+++ static/js/global.d.ts
@@ -24,7 +24,6 @@ declare let search_pill_widget: any;
 declare let settings: any;
 declare let settings_profile_fields: any;
 declare let settings_ui: any;
-declare let settings_user_groups: any;
 declare let stream_ui_updates: any;
 declare let message_view_header: any;
 declare let typing: any;
