4c625ebc8dd479b224cd2c7a7924ea373795762f
Anders Kaseorg <anders@zulip.com>
2020-09-24 11:24:24 +0000

    js: Convert static/js/feature_flags.js to ES6 module with modzulator.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- static/js/popovers.js
+++ static/js/popovers.js
@@ -16,6 +16,7 @@ const render_user_info_popover_title = require("../templates/user_info_popover_t
 const render_user_profile_modal = require("../templates/user_profile_modal.hbs");
 
 const blueslip = require("./blueslip");
+const feature_flags = require("./feature_flags");
 const people = require("./people");
 const settings_data = require("./settings_data");
 const util = require("./util");
