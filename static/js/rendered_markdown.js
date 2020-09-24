e87247ecc520631a40b9d9c02d8ec1bf3892eceb
Anders Kaseorg <anders@zulip.com>
2020-09-24 11:25:00 +0000

    js: Convert static/js/rendered_markdown.js to ES6 module with modzulator.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- static/js/rendered_markdown.js
+++ static/js/rendered_markdown.js
@@ -1,13 +1,11 @@
-"use strict";
+import ClipboardJS from "clipboard";
+import moment from "moment";
 
-const ClipboardJS = require("clipboard");
-const moment = require("moment");
+import copy_code_button from "../templates/copy_code_button.hbs";
 
-const copy_code_button = require("../templates/copy_code_button.hbs");
-
-const blueslip = require("./blueslip");
-const people = require("./people");
-const rtl = require("./rtl");
+import * as blueslip from "./blueslip";
+import * as people from "./people";
+import * as rtl from "./rtl";
 
 /*
     rendered_markdown
@@ -56,15 +54,15 @@ function get_user_group_id_for_mention_button(elem) {
 }
 
 // Helper function to update a mentioned user's name.
-exports.set_name_in_mention_element = function (element, name) {
+export function set_name_in_mention_element(element, name) {
     if ($(element).hasClass("silent")) {
         $(element).text(name);
     } else {
         $(element).text("@" + name);
     }
-};
+}
 
-exports.update_elements = (content) => {
+export const update_elements = (content) => {
     // Set the rtl class if the text has an rtl direction
     if (rtl.get_direction(content.text()) === "rtl") {
         content.addClass("rtl");
@@ -87,7 +85,7 @@ exports.update_elements = (content) => {
             if (person !== undefined) {
                 // Note that person might be undefined in some
                 // unpleasant corner cases involving data import.
-                exports.set_name_in_mention_element(this, person.full_name);
+                set_name_in_mention_element(this, person.full_name);
             }
         }
     });
