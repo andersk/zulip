0d78bc1acd1dce965c07575560f4036fdc76ab13
Anders Kaseorg <anders@zulip.com>
2022-06-04 23:07:57 -0700

    lazy emojisets

--- static/js/emojisets.js
+++ static/js/emojisets.js
@@ -1,44 +1,79 @@
 import google_blob_sheet from "emoji-datasource-google-blob/img/google/sheets-256/64.png";
 import google_sheet from "emoji-datasource-google/img/google/sheets-256/64.png";
 import twitter_sheet from "emoji-datasource-twitter/img/twitter/sheets-256/64.png";
+import {Observable} from "rxjs";
+import {distinctUntilChanged, switchMap} from "rxjs/operators";
 
 import octopus_url from "../generated/emoji/images-google-64/1f419.png";
 
 import {user_settings} from "./user_settings";
 
-import google_blob_css from "!style-loader?injectType=lazyStyleTag!css-loader!../generated/emoji-styles/google-blob-sprite.css";
-import google_css from "!style-loader?injectType=lazyStyleTag!css-loader!../generated/emoji-styles/google-sprite.css";
-import twitter_css from "!style-loader?injectType=lazyStyleTag!css-loader!../generated/emoji-styles/twitter-sprite.css";
-
 const emojisets = new Map([
-    ["google", {css: google_css, sheet: google_sheet}],
-    ["google-blob", {css: google_blob_css, sheet: google_blob_sheet}],
-    ["twitter", {css: twitter_css, sheet: twitter_sheet}],
+    [
+        "google",
+        {
+            css: () =>
+                import(
+                    "!style-loader?injectType=lazyStyleTag!css-loader!../generated/emoji-styles/google-sprite.css"
+                ),
+            sheet: google_sheet,
+        },
+    ],
+    [
+        "google-blob",
+        {
+            css: () =>
+                import(
+                    "!style-loader?injectType=lazyStyleTag!css-loader!../generated/emoji-styles/google-blob-sprite.css"
+                ),
+            sheet: google_blob_sheet,
+        },
+    ],
+    [
+        "twitter",
+        {
+            css: () =>
+                import(
+                    "!style-loader?injectType=lazyStyleTag!css-loader!../generated/emoji-styles/twitter-sprite.css"
+                ),
+            sheet: twitter_sheet,
+        },
+    ],
 ]);
 
 // For `text` emoji set we fallback to `google-blob` emoji set
 // for displaying emojis in emoji picker and typeahead.
 emojisets.set("text", emojisets.get("google-blob"));
 
-let current_emojiset;
+export let select;
+let current_css;
 
-export async function select(name) {
-    const new_emojiset = emojisets.get(name);
-    if (new_emojiset === current_emojiset) {
-        return;
-    }
-    await new Promise((resolve, reject) => {
-        const sheet = new Image();
-        sheet.addEventListener("load", resolve);
-        sheet.addEventListener("error", reject);
-        sheet.src = new_emojiset.sheet;
+new Observable((subscriber) => {
+    select = (name) => {
+        subscriber.next(name);
+    };
+})
+    .pipe(distinctUntilChanged())
+    .pipe(
+        switchMap(async (name) => {
+            const new_emojiset = emojisets.get(name);
+            const [new_css] = await Promise.all([
+                new_emojiset.css(),
+                new Promise((resolve, reject) => {
+                    const sheet = new Image();
+                    sheet.addEventListener("load", resolve);
+                    sheet.addEventListener("error", reject);
+                    sheet.src = new_emojiset.sheet;
+                }),
+            ]);
+            return new_css.default;
+        }),
+    )
+    .subscribe((new_css) => {
+        current_css?.unuse();
+        new_css.use();
+        current_css = new_css;
     });
-    if (current_emojiset) {
-        current_emojiset.css.unuse();
-    }
-    new_emojiset.css.use();
-    current_emojiset = new_emojiset;
-}
 
 export function initialize() {
     select(user_settings.emojiset);
